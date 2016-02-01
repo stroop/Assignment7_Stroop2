b40088bbf2ec6f5a30ec5db1909336b8aac466af
Jacob Stroop <israel941@gmail.com>
2016-01-31 19:24:44 -0500

    initial commit

---
+++ Assignment7_Stroop/Assignment7_Stroop.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7_Stroop
+{
+    class Assignment7_Stroop
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Invoice
+    {
+        // fields
+        private int id;
+        private double balance;
+        private int dueDate;
+
+        public Invoice(int id, double bal, int dueDate)
+        {
+            // thing
+        }
+
+        public int getID()
+        {
+            return this.id;
+        }
+        public 
+    }
+
+    enum Ranges
+    {
+        DUE_DATE_LOW = 1,
+        DUE_DATE_HIGH = 31,
+        ID_LOW = 100,
+        ID_HIGH = 999
+    }
+}
